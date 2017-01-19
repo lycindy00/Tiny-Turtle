733cb261f425d1686de28bffa98aa969fa09cb88
Cindy Ly <lycindy00@gmail.com>
2017-01-19 23:45:59 +0000

    Tiny turtle

---
+++ script.js
@@ -0,0 +1,17 @@
+/*global TinyTurtle*/
+ TinyTurtle.apply(window); 
+forward(50);
+right(90);
+forward(100);
+right(90);
+forward(50);
+right(90);
+forward(100);
+right(90);
+forward(50);
+right(65);
+forward(50);
+right(65);
+forward(100);
+
+stamp();
